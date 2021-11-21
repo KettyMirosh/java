5598b41ae0b75089ee07500e1a77eabd0081c20d
Ketty Mirosh <saturness@mail.ru>
2021-11-21 11:51:39 +0300

    Chnge main.java

--- src/com/itproger/Main.java
+++ src/com/itproger/Main.java
@@ -3,6 +3,7 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("hello");
+        // write your code here
     }
 }
