234b163ffd3f9e7b8efd0703698cf6ebec9a8340
Ketty Mirosh <saturness@mail.ru>
2021-11-21 11:57:15 +0300

    Adding new.gitignore

--- src/com/itproger/Main.java
+++ src/com/itproger/Main.java
@@ -3,7 +3,7 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello");
+	System.out.println("hello123");
         // write your code here
     }
 }
